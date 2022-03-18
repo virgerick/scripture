321b7643db9f2e36b4be02cefaa7608faebe08b9
Heriberto Alcantara Luna <heriberto.alcantara@uteco.edu.do>
2022-03-18 11:08:16 -0400

    change loadBible to load verses

--- pages/api/bible.ts
+++ pages/api/bible.ts
@@ -1,18 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { IBible } from "../../interfaces/IBible";
-import { loadBible, bibles } from "../../utilities/LoadBlible";
+import { loadVerses, loadBible, bibles } from "../../utilities/LoadBlible";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Array<IBible> | any>
 ) {
-  try {
-    if (bibles.length == 0) {
-      await loadBible();
-    }
-
-    res.status(200).json(bibles);
-  } catch (error) {
-    res.status(400).json(error);
+  if (bibles.length == 0) {
+    //await loadBible();
+    const verser = await loadVerses({
+      language: "Spanish",
+      translation: "Reina Valera (1909)",
+      abbreviation: "valera",
+      textdirection: "LTR",
+      filename: "Spanish__Reina_Valera_(1909)__valera__LTR",
+      hash: "268a947e3796c99ce87ba1af722253d2c7b51739",
+      id: "94",
+    });
   }
+  res.status(200).json(bibles);
 }
