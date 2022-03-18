0e8cc77d94a93afa31f8786674fa9257fa7d5fe4
Heriberto Alcantara Luna <heriberto.alcantara@uteco.edu.do>
2022-03-18 11:25:11 -0400

    unneeded import removed

--- pages/api/translations.ts
+++ pages/api/translations.ts
@@ -1,11 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ITranslation } from "../../interfaces/ITranslations";
-import { translations } from "../../utilities/LoadBlible";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Array<ITranslation> | any>
 ) {
-  const translation = require("../../Assets/translations.json");
+  const translations = require("../../Assets/translations.json");
   res.status(200).json(Object.values(translations));
 }
