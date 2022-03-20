6a4135ed2be193abb9aaeaf85b51fc5af1e3e70c
Viregerick <elfeo.0103@gmail.com>
2022-03-19 21:24:36 -0400

    test onclic event

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { MouseEventHandler, useState } from "react";
+import {useState } from "react";
 import { ITranslation } from "../interfaces/ITranslations";
 import styles from "../styles/Home.module.css";
 interface Prop {
@@ -10,7 +10,7 @@ interface Prop {
 const Home: NextPage<Prop> = (props: Prop) => {
   const { translations } = props;
   const [translation,setTranslation]=useState<string>("valera")
-  const handlerSearch=(e:MouseEventHandler<HTMLButtonElement>)=>{
+  const handlerSearch=()=>{
     console.log(translation);
 
   }
