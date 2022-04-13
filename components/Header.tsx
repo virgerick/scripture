911128e874fd9b4ef281c69b269025d6d7f221df
Viregerick <elfeo.0103@gmail.com>
2022-04-13 17:19:36 -0400

    removing a tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 
+import Link from "next/link";
 import styles from "../styles/layout.module.css";
 import { SelectLanguage } from "./SelectLanguage";
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <a href="/">Scripture</a>
+      <Link href="/">Scripture</Link>
 
       <SelectLanguage />
     </header>
