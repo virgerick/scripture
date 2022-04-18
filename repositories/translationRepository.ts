29fa880076c6b1338a55ee4e164a16e08d18809f
Viregerick <elfeo.0103@gmail.com>
2022-04-18 19:41:03 -0400

    using fs

--- repositories/translationRepository.ts
+++ repositories/translationRepository.ts
@@ -4,21 +4,14 @@ export default class TranslationRepository {
   translations: Array<ITranslation> = [];
   constructor() {
     // this.translations = Object.values(require("../Public/Assets/translations.json"));
-
-    this.load();
+  const file=  readFileSync("./public/Assets/translations.json","utf8");
+    this.translations = Object.values(JSON.parse(file));
   }
   get() {
     return this.translations;
   }
   getById(id: string) {
     return this.translations.find((t) => t.id == id);
-  }
-  async load() {
-    const file = await readFileSync(
-      "./public/Assets/translations.json",
-      "utf8"
-    );
-    this.translations =  Object.values(JSON.parse(file));
   }
 }
 const translationRepository = new TranslationRepository();
